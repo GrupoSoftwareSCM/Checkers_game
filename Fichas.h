b1301a7232c3dc868a6fd1f8c2e4fb042f388f2b
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2015-05-27 18:10:02 -0300

    agregue atributos de posicion

--- Fichas.h
+++ Fichas.h
@@ -15,17 +15,23 @@ class Fichas {
 private:
     string Ficha;
     string jugador;
+    int posH;
+    int posV;
     
 public:
     Fichas();
     
     string getFichas();
     string getJugador();
+    int getPosh();
+    int getPosv();
     
     void setFichas(string);
     void setJugador(string);
+    void setPosh(int);
+    void setPosv(int);
     
-    string toString();
+    void listar();
 };
 
 #endif	/* FICHAS_H */
